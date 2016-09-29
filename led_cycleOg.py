7cad19e1b67280aacf33c740a88ce8daabf9a566
Firstname Surname <fislam@itcollege.ee>
2016-09-29 12:01:02 +0000

    working cycle

---
+++ led_cycleOg.py
@@ -0,0 +1,37 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+t = 0.1
+pr = GPIO.PWM(10, 400)  # channel=12 frequency=50Hz
+pb = GPIO.PWM(27, 400) 
+pg = GPIO.PWM(22, 400) 
+pr.start(0)
+pb.start(0)
+pg.start(0)
+try:
+    while 1:
+        for dc in range(1, 101, 5):
+            pr.ChangeDutyCycle(dc)
+            time.sleep(t)
+	for dc in range(1, 101, 5):
+            pb.ChangeDutyCycle(dc)
+            time.sleep(t)
+	for dc in range(1, 101, 5):
+            pg.ChangeDutyCycle(dc)
+            time.sleep(t)
+        for dc in range(100, -1, -5):
+            pr.ChangeDutyCycle(dc)
+            time.sleep(t)
+        for dc in range(100, -1, -5):
+            pb.ChangeDutyCycle(dc)
+            time.sleep(t)
+        for dc in range(100, -1, -5):
+            pg.ChangeDutyCycle(dc)
+            time.sleep(t)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
